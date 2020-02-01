5024f114b12ee440c4d0c4727bae10eb45e639ca
shreejoy-projects <56832212+shreejoy-projects@users.noreply.github.com>
2020-02-01 09:36:42 +0530

    Create Q2.java

---
+++ DSA-A-2/Q2.java
@@ -0,0 +1,38 @@
+public class Q2 {
+
+	public static void main(String[] args) {
+		Complex c1 = new Complex();
+		Complex c2 = new Complex();
+
+		c1.setData(5, 6);
+		c1.display();
+		c2.setData(2, 3);
+		c2.display();
+		Complex c3 = new Complex();
+		
+		c3 = c1.add(c1, c2);
+		c3.display();
+	}
+
+}
+class Complex {
+	int real;
+	int imag;
+	
+	void setData(int r, int i)
+	{
+		real = r;
+		imag = i;
+	}
+	void display() {
+		System.out.println(real+" + "+imag+"i");
+	}
+	
+	Complex add(Complex cx, Complex cy)
+	{	
+		Complex c3 = new Complex();
+		c3.real = cx.real + cy.real;
+		c3.imag = cx.imag + cy.imag;
+		return c3;
+	}
+}
