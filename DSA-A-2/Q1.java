cb6c7e3e1354fa6c05f7ea2c974c560a594a13e5
shreejoy-projects <56832212+shreejoy-projects@users.noreply.github.com>
2020-02-01 09:41:20 +0530

    Update Q1.java

--- DSA-A-2/Q1.java
+++ DSA-A-2/Q1.java
@@ -1,3 +1,21 @@
+/*
+A sales person is paid commission based on the sales he makes as shown by the following
+table:
+                                           SALE  | COMMISSION
+					   ------------------
+                                   Under Rs. 500 | 2% of SALES
+                      Rs. 500 and under Rs. 5000 | 5% of SALES
+                               Rs. 5000 and over | 8% of SALES
+Write a class, Commission, which has:
+* An instance variable, sales; an appropriate constructor; and a method, getCommission()
+that returns the commission.
+
+Now write a Demo class in Java to test the Commission class by reading a sale from the
+user, using it to create a Commission object after validating that the value is not negative.
+Finally, call the getcommission() method to get and print the commission. If the sales are
+negative, your Demo class should print the message “Invalid Input”.
+*/
+
 import java.util.*;
 public class Q1 {
 
